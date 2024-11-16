05aea02a3217775c12a1831a1d6985923132ed6e
Achraf Mhadhbi <mhadhbi.achref@gmail.com>
2024-11-16 23:54:35 +0100

    Create dynamic task

--- app.js
+++ app.js
@@ -7,10 +7,13 @@ class Root extends Component {
     
     <div class="input-group-lg bg-white rounded border d-flex w-100 align-items-center">
         <input type="name" class="form-control-lg fs-5 flex-fill border-0" placeholder="Add your new task"
-               aria-label="Add your new task" id="name" name="name" aria-describedby="button-addon2"/>
+               aria-label="Add your new task" id="name" name="name" aria-describedby="button-addon2"
+               t-att-value="state.name" t-model="state.name"/>
         <input type="color" class="form-control-lg border-0 bg-white m-0 form-control-color" id="color"
-               value="#563d7c" title="Choose your color"/>
-        <button class="btn btn-primary" type="button" id="button-addon2"><i class="bi bi-plus-lg fs-3"></i>
+               value="#563d7c" title="Choose your color"
+               t-att-value="state.color" t-model="state.color"/>
+        <button class="btn btn-primary" type="button" id="button-addon2"
+               t-on-click="addTask"><i class="bi bi-plus-lg fs-3"></i>
         </button>
     </div>
 
@@ -35,16 +38,41 @@ class Root extends Component {
     </ul>
 </div>
   
-
 `
-setup(){
-    this.tasks = useState([
-      {id:1, name:"Task 1", color:"fff000", isCompleted: false},
-      {id:2, name:"Task 2", color:"fff000", isCompleted: false},
-      {id:3, name:"Task 3", color:"fff000", isCompleted: false},
 
-    ])
-  }
+
+    setup(){
+            this.state = useState({
+            name: "",
+            color: "#FFF700",
+            isCompleted: false,
+            isEditing: false
+        });
+
+        this.tasks = useState([])
+    }
+
+    addTask(){
+        // do not allow empty task name
+        if (!this.state.name){
+            alert("Please add task.")
+            return
+        }
+
+        // add unique id
+        const id = Math.random().toString().substring(2,12)
+        
+        this.tasks.push({
+            id,
+            name: this.state.name,
+            color: this.state.color,
+            isCompleted: this.state.isCompleted,
+        })
+
+        // reset states after saving
+        let state = this.state
+        this.state = {...state, name:"", color: "#FFF700"}
+    }
 
 }
 mount(Root, document.getElementById("root"))
